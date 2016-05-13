92fecf72a60c12e3ad705a1f39f020d7751c38a1
neburor <ruben.couoh@live.com.mx>
2016-05-13 16:26:39 -0500

    Delete config.php

--- global/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-#Config
-define('UMSDOMAIN', 'hostingmex.com.mx');
-define('URLSYSTEM', 'http://ums.hostingmex.com.mx/');
-define('FBAPPID', '743180439142490');
-define('FBAPPSECRET', '48d1214467c1eaed4ba6e13280bf60e9');
-define('TWCONSUMERKEY', '2Pq4xUfSes1B2RvIENXHXs3qC');
-define('TWCONSUMERSECRET', 'JcI9hqtowv0aEzdUeXJcT0mZXRCJ1AgMIoz6QwFtAPLXCAntzF');
